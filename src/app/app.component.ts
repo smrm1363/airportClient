935479f5bea5d272d0c4fb59cdfe817999201017
Mohammadreza <smrm1363@gmail.com>
2019-02-09 21:27:48 +0330

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {Component} from "@angular/core";
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'airportClient';
+  @ViewChild(NgAutoCompleteComponent) public completer: NgAutoCompleteComponent;
+
+  public group = [
+    CreateNewAutocompleteGroup(
+      'Search / choose in / from list',
+      'completer',
+      [
+        {title: 'Option 1', id: '1'},
+        {title: 'Option 2', id: '2'},
+        {title: 'Option 3', id: '3'},
+        {title: 'Option 4', id: '4'},
+        {title: 'Option 5', id: '5'},
+      ],
+      {titleKey: 'title', childrenKey: null}
+    ),
+  ];
+
+  constructor() {
+
+  }
+
+  /**
+   *
+   * @param item
+   * @constructor
+   */
+  Selected(item: SelectedAutocompleteItem) {
+    console.log(item);
+  }
 }
